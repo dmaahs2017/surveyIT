07f6752013316d25ff44645886a2bcc3d208f723
Dalton Maahs <maahs2017@gmail.com>
2020-09-07 20:03:03 -0500

    gitignore migrations

--- server/src/migrations/Migration20200908005129.ts
+++
@@ -1,9 +0,0 @@
-import { Migration } from '@mikro-orm/migrations';
-
-export class Migration20200908005129 extends Migration {
-
-  async up(): Promise<void> {
-    this.addSql('drop table if exists "post" cascade;');
-  }
-
-}
