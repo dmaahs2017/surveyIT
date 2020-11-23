807cf3f2e637f2c0e17446405465aab5818524c3
Dalton Maahs <dalton.maahs@yourbind.com>
2020-11-23 11:56:22 -0600

    delete test file

--- web/src/pages/test.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { Box } from "@chakra-ui/core";
-import { QuestionResponse } from "../components/QuestionResopnse";
-const Test = () => <Box>{QuestionResponse(1, 3)}</Box>;
-
-export default Test;
